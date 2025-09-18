c69e023d0bfcbed95b8e86e9d068e7faae5f617c
Rodrigo Silva <souza.rodrigo11@gmail.com>
2025-09-18 12:01:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults({ static: 'public' });
 
-// middlewares (logger, CORS, servir /public, etc.)
 server.use(middlewares);
 
-// rota para retornar o DB completo (útil pra testar no navegador)
 server.get('/db', (req, res) => {
   res.jsonp(router.db.getState());
 });
 
-// demais rotas do db.json (/services, /testimonials, ...)
 server.use(router);
 
 const port = process.env.PORT || 3001;
