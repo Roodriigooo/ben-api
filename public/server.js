8d3daae6f3b21157c0bcbc1665c2e86c1845c485
Rodrigo Silva <souza.rodrigo11@gmail.com>
2025-09-18 09:35:50 -0300

    Update server.js

--- public/server.js
+++ public/server.js
@@ -1,19 +1,19 @@
-// server.js
+// server.js (ESM)
 import jsonServer from 'json-server';
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({ static: 'public' });
 
-// middlewares (logger, CORS, static /public, etc.)
+// middlewares (logger, CORS, /public, etc.)
 server.use(middlewares);
 
-// Rota manual para retornar o conteúdo completo do banco
+// rota para retornar o DB completo
 server.get('/db', (req, res) => {
   res.jsonp(router.db.getState());
 });
 
-// Demais rotas geradas a partir do db.json
+// demais rotas do db.json
 server.use(router);
 
 const port = process.env.PORT || 3001;
